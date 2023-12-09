5b5111fc5ed7cac05eb5bdf0f062fd622dd19d19
Tanju BOZOK <tanjubozok@hotmail.com>
2023-12-10 02:37:40 +0300

    view bas yapısı eklendi.

---
+++ JobTracking.WebUi/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using JobTracking.Business.Services;
+using JobTracking.Common.ComplexTypes;
+
+namespace JobTracking.WebUi.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CategoryController : Controller
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoryController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    public async Task<IActionResult> List()
+    {
+        var result = await _categoryService.GetCategoryListAsync();
+        if (result.ResponseType == ResponseType.Success)
+            return View(result.Data);
+        return View();
+    }
+}
--- JobTracking.WebUi/Program.cs
+++ JobTracking.WebUi/Program.cs
@@ -29,6 +29,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "areaRoute",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
